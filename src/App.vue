604556720421bd9b0981512528edaa19774e570e
stefanom13 <stefanomancuso13@gmail.com>
2022-03-21 14:21:41 +0100

    inserimento header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyHeader,
   }
 }
 </script>
 
 <style lang="scss">
+@import './assets/_style.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="hello">
+    <div class="container-fluid">
+      <div class="container">
+        <!-- navbar -->
+        <div class="row d-row">
+          <figure>
+            <img src="../assets/img/logo.png" alt="">
+          </figure>
+          <!--container list  -->
+          <div class="container-list">
+
+            <ul class="list-ul">
+              <li>
+                ciao
+              </li>
+            </ul>
+            <button class="btn">Join us</button>
+            <p>icona</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container-fluid{
+  background-color: #27aabe;
+  min-height:60px;
+  display:flex;
+  align-items: center;
+ 
+}
+
+.container{
+  width: 1000px;
+
+  .d-row{
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 15px;
+
+    figure{
+      padding:10px;
+
+      img{
+        width:80%;
+      }
+    }
+    .container-list{
+      display:flex;
+      align-items: center;
+      gap: 30px;
+
+      .btn{
+        background-color: black;
+        padding: 10px 12px;
+        font-weight:700;
+        color:white;
+        font-size: 10px;
+        border:none;
+        cursor: pointer;
+        text-transform: uppercase;
+       
+      }
+    }
+    .list-ul{
+     display:flex;
+     gap: 25px;
+    }
+   
+  }
+
+  
+}
+</style>
